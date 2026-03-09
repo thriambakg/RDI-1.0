4962846136b667c801207c6eae2a25ab201cea80
Thri Giri <thriambak.g@gmail.com>
2026-03-09 15:49:08 -0500

    first commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,7 +34,6 @@ locals {
   region            = data.aws_region.current.name
 }
 
-
 output "account_id" {
   description = "Current AWS account ID"
   value       = data.aws_caller_identity.current.account_id
