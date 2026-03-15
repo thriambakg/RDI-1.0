d1d85ab2ff37d751e5a9be62ac4d1a5c48ebe6e9
Thri Giri <thriambak.g@gmail.com>
2026-03-15 01:45:07 -0500

    pinging

--- terraform/modules/proxy-ec2/main.tf
+++ terraform/modules/proxy-ec2/main.tf
@@ -103,7 +103,7 @@ resource "aws_security_group" "proxy" {
   }
 
   ingress {
-    description = "Session status API (Lambda -> proxy instructions)"
+    description = "Session status API Lambda to proxy instructions"
     from_port   = var.proxy_status_port
     to_port     = var.proxy_status_port
     protocol    = "tcp"
