d2c922b556b9d793c5d8bf3d6f264716f26cf3be
Thri Giri <thriambak.g@gmail.com>
2026-03-17 00:10:15 -0500

    pinging

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -79,7 +79,7 @@ variable "skip_agent_build" {
 variable "infra_version" {
   description = "Version number for proxy/ALB/Wavelength infra. Set to 0 to destroy (count=0); set to 1 or higher to create. Bump the value (e.g. 1 -> 2) and apply to force replacement of instances and ALB."
   type        = number
-  default     = 5
+  default     = 6
 }
 
 variable "proxy_subnet_cidr" {
