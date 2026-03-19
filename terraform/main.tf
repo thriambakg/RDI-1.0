69de559d4915c1e35b8177492b3b807eee65d739
Thri Giri <thriambak.g@gmail.com>
2026-03-19 17:59:30 -0500

    reset

--- terraform/main.tf
+++ terraform/main.tf
@@ -460,6 +460,7 @@ module "session_api" {
     }
   }
 
+
   lambda_permissions = {
     post               = { function_arn = module.session_api_lambda[0].function_arn, http_method = "POST", resource_path = "sessions" }
     get                = { function_arn = module.session_api_lambda[0].function_arn, http_method = "GET", resource_path = "sessions" }
